1b1e294aec3ed0db86d57bfaa75a39c2b2be1c45
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-04-27 07:46:02 -0500

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import { Link } from 'react-router-dom'
+
+
+function Sidebar() {
+  return (
+      <div>
+        
+      </div>
+  )
+}
+
+export default Sidebar
